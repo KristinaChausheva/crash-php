b9900bda30bf4cdf639e60daa5bb5a864551484e
Kristina Chausheva <k.chausheva@gmail.com>
2022-05-19 02:16:58 +0300

    outputs and variables

---
+++ 01_output.php
@@ -0,0 +1,15 @@
+<?php
+// echo - outputs strings, numbers, html, etc
+// echo 123, "hello", 10.5
+
+// print - works like echo but can only take in a single argument
+// print 123
+
+// print_r() - prints single values and arrays
+print_r([1,2,3])
+
+//var_dump() - returns more info like data type and length
+
+//var_export() - similar to var_dump() outputs a string representation of a variable
+
+?>
